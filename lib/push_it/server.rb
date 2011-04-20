763353a4040f6dd5a5c4d382500fd0b5e6a494a9
Bryan Helmkamp <bryan@brynary.com>
2011-04-20 18:05:27 -0400

    Add dumb capistrano running

--- lib/push_it/server.rb
+++ lib/push_it/server.rb
@@ -1,6 +1,23 @@
 require "sinatra/base"
+require "posix/spawn"
 
 module PushIt
   class Server < Sinatra::Base
+
+    post "/deploy" do
+      Thread.new { deploy! }
+      "started"
+    end
+
+    def deploy!
+      child = POSIX::Spawn::Child.new(*command.split)
+      puts child.out
+      raise child.err unless child.status.success?
+    end
+
+    def command
+      "cap staging deploy"
+    end
+
   end
 end
