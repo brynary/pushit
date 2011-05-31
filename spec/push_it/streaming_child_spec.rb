fb52a7fa500a0f626512a4485fd23dbac7864174
Noah Davis <noah@efficiency20.com>
2011-05-31 17:25:52 -0400

    port test/unit tests to rspec
    tests previously were not running

--- spec/push_it/streaming_child_spec.rb
+++ spec/push_it/streaming_child_spec.rb
@@ -1,86 +1,88 @@
 require 'spec_helper'
+require 'test/unit'
 
 module PushIt
 
   describe StreamingChild do
+    include Test::Unit::Assertions
 
-    def test_argv_array_execs
+    it "test_argv_array_execs" do
       p = StreamingChild.new('printf', '%s %s %s', '1', '2', '3 4')
       assert p.success?
       assert_equal "1 2 3 4", p.out
     end
 
-    def test_argv_string_uses_sh
+    it "test_argv_string_uses_sh" do
       p = StreamingChild.new("echo via /bin/sh")
       assert p.success?
       assert_equal "via /bin/sh\n", p.out
     end
 
-    def test_stdout
+    it "test_stdout" do
       p = StreamingChild.new('echo', 'boom')
       assert_equal "boom\n", p.out
       assert_equal "", p.err
     end
 
-    def test_stderr
+    it "test_stderr" do
       p = StreamingChild.new('echo boom 1>&2')
       assert_equal "", p.out
       assert_equal "boom\n", p.err
     end
 
-    def test_status
+    it "test_status" do
       p = StreamingChild.new('exit 3')
       assert !p.status.success?
       assert_equal 3, p.status.exitstatus
     end
 
-    def test_env
+    it "test_env" do
       p = StreamingChild.new({ 'FOO' => 'BOOYAH' }, 'echo $FOO')
       assert_equal "BOOYAH\n", p.out
     end
 
-    def test_chdir
+    it "test_chdir" do
       p = StreamingChild.new("pwd", :chdir => File.dirname(Dir.pwd))
       assert_equal File.dirname(Dir.pwd) + "\n", p.out
     end
 
-    def test_input
+    it "test_input" do
       input = "HEY NOW\n" * 100_000 # 800K
       p = StreamingChild.new('wc', '-l', :input => input)
       assert_equal 100_000, p.out.strip.to_i
     end
 
-    def test_max
-      assert_raise MaximumOutputExceeded do
+    it "test_max" do
+      assert_raise POSIX::Spawn::MaximumOutputExceeded do
         StreamingChild.new('yes', :max => 100_000)
       end
     end
 
-    def test_max_with_child_hierarchy
-      assert_raise MaximumOutputExceeded do
+    it "test_max_with_child_hierarchy" do
+      assert_raise POSIX::Spawn::MaximumOutputExceeded do
         StreamingChild.new('/bin/sh', '-c', 'yes', :max => 100_000)
       end
     end
 
-    def test_max_with_stubborn_child
-      assert_raise MaximumOutputExceeded do
+    it "test_max_with_stubborn_child" do
+      assert_raise POSIX::Spawn::MaximumOutputExceeded do
         StreamingChild.new("trap '' TERM; yes", :max => 100_000)
       end
     end
 
-    def test_timeout
-      assert_raise TimeoutExceeded do
+    it "test_timeout" do
+      assert_raise POSIX::Spawn::TimeoutExceeded do
         StreamingChild.new('sleep', '1', :timeout => 0.05)
       end
     end
 
-    def test_timeout_with_child_hierarchy
-      assert_raise TimeoutExceeded do
+    it "test_timeout_with_child_hierarchy" do
+      assert_raise POSIX::Spawn::TimeoutExceeded do
         StreamingChild.new('/bin/sh', '-c', 'sleep 1', :timeout => 0.05)
       end
     end
 
-    def test_lots_of_input_and_lots_of_output_at_the_same_time
+    it "test_lots_of_input_and_lots_of_output_at_the_same_time" do
       input = "stuff on stdin \n" * 1_000
       command = "
         while read line
@@ -95,13 +97,13 @@ module PushIt
       assert p.success?
     end
 
-    def test_input_cannot_be_written_due_to_broken_pipe
+    it "test_input_cannot_be_written_due_to_broken_pipe" do
       input = "1" * 100_000
       p = StreamingChild.new('false', :input => input)
       assert !p.success?
     end
 
-    def test_utf8_input
+    it "test_utf8_input" do
       input = "hålø"
       p = StreamingChild.new('cat', :input => input)
       assert p.success?
